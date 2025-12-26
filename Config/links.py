b64efb648ad71571e3d79b8c63c1401eadc88f6e
unknown <sasha.pop.2019@gmail.com>
2025-12-26 18:27:46 +0100

    delete old cache

--- Config/links.py
+++
@@ -1,6 +0,0 @@
-
-class links:
-    HOST = "https://opensource-demo.orangehrmlive.com/web/index.php"
-    LOGIN_PAGE = f"{HOST}/auth/login"
-    DASHBOARD_PAGE = f"{HOST}/dashboard/index"
-    BUZZ_PAGE = f"{HOST}/buzz/viewBuzz"
